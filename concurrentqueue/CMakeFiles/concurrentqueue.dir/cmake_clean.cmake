a71a4e809206febbaed4303674da09ec60151817
AlphaCat00 <00.alpha.cat@gmail.com>
2021-12-11 07:52:29 -0800

    rewrite kv_app_send using concurrentqueue

---
+++ concurrentqueue/CMakeFiles/concurrentqueue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/concurrentqueue.dir/concurrentqueue.cpp.o"
+  "libconcurrentqueue.pdb"
+  "libconcurrentqueue.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/concurrentqueue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- kv/kv_app.c
+++ kv/kv_app.c
@@ -2,49 +2,39 @@
 
 #include <pthread.h>
 #include <stdio.h>
-#include <sys/queue.h>
 
+#include "concurrentqueue.h"
 #include "memery_operation.h"
 #include "spdk/env.h"
 #include "spdk/event.h"
 #include "spdk/thread.h"
 
+pthread_mutex_t g_lock;
 static struct kv_app_t g_app;
-struct msg_entry {
-    kv_app_func func;
-    void *arg;
-    STAILQ_ENTRY(msg_entry) next;
-};
 
 struct thread_data {
-    pthread_mutex_t lock;
-    STAILQ_HEAD(, msg_entry) msgs;
+    MoodycamelCQHandle cq;
     struct spdk_poller *poller;
 } g_threads[MAX_TASKS_NUM];
 
 const struct kv_app_t *kv_app(void) { return &g_app; }
 
 void kv_app_send(uint32_t index, kv_app_func func, void *arg) {
-    struct msg_entry *msg = kv_malloc(sizeof(struct msg_entry));
-    *msg = (struct msg_entry){func, arg};
-    pthread_mutex_lock(&g_threads[index].lock);
-    STAILQ_INSERT_TAIL(&g_threads[index].msgs, msg, next);
-    pthread_mutex_unlock(&g_threads[index].lock);
+    struct kv_app_task *msg = kv_malloc(sizeof(struct kv_app_task));
+    *msg = (struct kv_app_task){func, arg};
+    moodycamel_cq_enqueue(g_threads[index].cq, msg);
 }
 
+#define MAX_POLL_SZ 16
 static int msg_poller(void *arg) {
     struct thread_data *data = arg;
-    while (true) {
-        pthread_mutex_lock(&data->lock);
-        if (STAILQ_EMPTY(&data->msgs)) {
-            pthread_mutex_unlock(&data->lock);
-            break;
+    struct kv_app_task *msg[MAX_POLL_SZ];
+    size_t size;
+    while ((size = moodycamel_cq_try_dequeue_bulk(data->cq, (MoodycamelValue *)msg, MAX_POLL_SZ))) {
+        for (size_t i = 0; i < size; i++) {
+            if (msg[i]->func) msg[i]->func(msg[i]->arg);
+            kv_free(msg[i]);
         }
-        struct msg_entry *msg = STAILQ_FIRST(&data->msgs);
-        STAILQ_REMOVE_HEAD(&data->msgs, next);
-        pthread_mutex_unlock(&data->lock);
-        if (msg->func) msg->func(msg->arg);
-        kv_free(msg);
     }
     return 0;
 }
@@ -65,11 +55,13 @@ static void register_func(void *arg) {
     uint32_t index;
     if (sscanf(spdk_thread_get_name(thread), "reactor_%u", &index) == 1) {
         g_app.threads[index] = thread;
+        moodycamel_cq_create(&g_threads[index].cq);
         g_threads[index].poller = spdk_poller_register(msg_poller, g_threads + index, 0);
     }
 }
 static void send_msg_to_all(void *arg) {
     g_app.threads[0] = spdk_get_thread();
+    moodycamel_cq_create(&g_threads[0].cq);
     g_threads[0].poller = spdk_poller_register(msg_poller, g_threads, 0);
     spdk_for_each_thread(register_func, arg, app_start);
 }
@@ -85,40 +77,29 @@ int kv_app_start(const char *json_config_file, uint32_t task_num, struct kv_app_
     opts.name = "kv_app";
     opts.reactor_mask = cpu_mask;
     opts.json_config_file = json_config_file;
-    for (size_t i = 0; i < task_num; i++) {
-        pthread_mutex_init(&g_threads[i].lock, NULL);
-        STAILQ_INIT(&g_threads[i].msgs);
-    }
+    pthread_mutex_init(&g_lock, NULL);
     int rc = 0;
     if ((rc = spdk_app_start(&opts, send_msg_to_all, tasks))) SPDK_ERRLOG("ERROR starting application\n");
+    for (size_t i = 0; i < task_num; i++) moodycamel_cq_destroy(g_threads[i].cq);
     spdk_app_fini();
     return rc;
 }
 
-struct app_stop_arg {
+void kv_app_stop(int rc) {
+    const struct spdk_thread *thread = spdk_get_thread();
     uint32_t index;
-    int rc;
-};
-
-static void app_stop(void *_arg) {
-    struct app_stop_arg *arg = _arg;
+    for (index = 0; g_app.threads[index] != thread; ++index)
+        ;
+    spdk_poller_unregister(&g_threads[index].poller);
+    pthread_mutex_lock(&g_lock);
     if (g_app.running_thread) {
-        if (arg->rc) {
-            spdk_app_stop(arg->rc);
+        if (rc) {
+            spdk_app_stop(rc);
             g_app.running_thread = 0;
         } else {
-            g_app.running_thread &= ~(1ULL << arg->index);
-            if (!g_app.running_thread) spdk_app_stop(arg->rc);
+            g_app.running_thread &= ~(1ULL << index);
+            if (!g_app.running_thread) spdk_app_stop(rc);
         }
     }
-    kv_free(arg);
-}
-void kv_app_stop(int rc) {
-    struct app_stop_arg *arg = kv_malloc(sizeof(struct app_stop_arg));
-    const struct spdk_thread *thread = spdk_get_thread();
-    for (arg->index = 0; g_app.threads[arg->index] != thread; ++arg->index)
-        ;
-    arg->rc = rc;
-    spdk_poller_unregister(&g_threads[arg->index].poller);
-    kv_app_send(0, app_stop, arg);
+    pthread_mutex_unlock(&g_lock);
 }
