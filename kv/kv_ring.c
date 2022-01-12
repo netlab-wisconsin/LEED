746628bab52f375456f36c2e085125be713b8581
AlphaCat00 <00.alpha.cat@gmail.com>
2022-01-12 07:07:03 -0800

    kv_ring: connection management.

--- kv/kv_ring.c
+++ kv/kv_ring.c
@@ -16,6 +16,8 @@
 struct kv_node {
     struct kv_node_info *info;
     connection_handle conn;
+    bool is_connected, is_disconnecting, is_local;
+    STAILQ_ENTRY(kv_node) next;
     UT_hash_handle hh;
 };
 struct vid_entry {
@@ -27,13 +29,17 @@ CIRCLEQ_HEAD(vid_ring, vid_entry);
 
 struct kv_ring {
     kv_rdma_handle h;
+    uint32_t thread_id;
     char *local_ip;
     char *local_port;
     pthread_rwlock_t lock;
     struct kv_node *nodes;
     struct vid_ring *rings;
     pthread_mutex_t rings_lock;
+    STAILQ_HEAD(, kv_node) conn_q;
     // ssd_status
+    kv_ring_cb ready_cb;
+    void *arg;
 } g_ring;
 
 static void random_vid(char *vid) {
@@ -85,11 +91,55 @@ void kv_ring_dispatch(char *key, connection_handle *h, uint32_t *ssd_id) {
     *h = entry->node->conn;
     *ssd_id = entry->vid->ssd_id;
 }
+static void rdma_disconnect_cb(void *arg) {
+    struct kv_node *node = arg;
+    free(node->info);  // info is allocated by libkv_etcd
+    kv_free(node);
+}
+static void rdma_connect_cb(connection_handle h, void *arg) {
+    struct kv_ring *self = &g_ring;
+    struct kv_node *node = arg;
+    assert(!node->is_local);
+    if (!h) fprintf(stderr, "can't connect to node %s:%s, retrying ...\n", node->info->rdma_ip, node->info->rdma_port);
+    if (node->is_disconnecting || !h) {
+        // disconnect or retry
+        STAILQ_INSERT_TAIL(&self->conn_q, node, next);
+        return;
+    }
+    printf("connected to node %s:%s.\n", node->info->rdma_ip, node->info->rdma_port);
+    node->is_connected = true;
+    node->conn = h;
+}
 
-static void node_handler(struct kv_node_info *info) {
+static int kv_conn_q_poller(void *arg) {
+    struct kv_ring *self = arg;
+    while (!STAILQ_EMPTY(&self->conn_q)) {
+        struct kv_node *node = STAILQ_FIRST(&self->conn_q);
+        STAILQ_REMOVE_HEAD(&self->conn_q, next);
+        assert(!node->is_local);
+        if (node->is_disconnecting) {
+            if (node->is_connected)
+                kv_rdma_disconnect(node->conn, rdma_disconnect_cb, node);
+            else
+                rdma_disconnect_cb(node);
+        } else {
+            assert(!node->is_connected);
+            kv_rdma_connect(self->h, node->info->rdma_ip, node->info->rdma_port, rdma_connect_cb, node);
+        }
+    }
+    if (self->ready_cb) {
+        for (struct kv_node *node = self->nodes; node != NULL; node = node->hh.next)
+            if (!node->is_local && !node->is_disconnecting && !node->is_connected) return 0;
+        self->ready_cb(self->arg);
+        self->ready_cb = NULL;  // call ready_cb only once
+    }
+    return 0;
+}
+
+static void node_handler(void *arg) {
+    struct kv_node_info *info = arg;
     struct kv_ring *self = &g_ring;
     vid_ring_init(info->vid_num);
-    bool is_local = self->local_ip && strcmp(self->local_ip, info->rdma_ip) && strcmp(self->local_port, info->rdma_port);
     if (info->msg_type == KV_NODE_INFO_DELETE) {
         struct kv_node *node = NULL;
         pthread_rwlock_wrlock(&self->lock);
@@ -103,18 +153,21 @@ static void node_handler(struct kv_node_info *info) {
         }
         HASH_DEL(self->nodes, node);
         pthread_rwlock_unlock(&self->lock);
-        kv_free(node);
-        free(info);
-        if (is_local) {
+        if (node->is_local) {
             fprintf(stderr, "kv_etcd keepalive timeout!\n");
             exit(-1);
         } else {
-            // rdma disconnect
+            node->is_disconnecting = true;
+            if (node->is_connected) STAILQ_INSERT_TAIL(&self->conn_q, node, next);
         }
     } else {
         struct kv_node *node = kv_malloc(sizeof(struct kv_node));
+        node->is_local = self->local_ip && !strcmp(self->local_ip, info->rdma_ip) && !strcmp(self->local_port, info->rdma_port);
+        node->is_disconnecting = false;
+        node->is_connected = false;
         node->info = info;
         pthread_rwlock_wrlock(&self->lock);
+        // node may already exist?
         HASH_ADD(hh, self->nodes, info->rdma_ip, 24, node);  // ip & port as key
         for (size_t i = 0; i < info->vid_num; i++) {         // add vids to ring
             struct vid_entry *entry = kv_malloc(sizeof(struct vid_entry)), *x;
@@ -127,9 +180,19 @@ static void node_handler(struct kv_node_info *info) {
             }
         }
         pthread_rwlock_unlock(&self->lock);
-        if (!is_local) {
-            // rdma connnect
+        if (!node->is_local) {
+            STAILQ_INSERT_TAIL(&self->conn_q, node, next);
         }
+    }
+}
+
+static void _node_handler(struct kv_node_info *info) {
+    struct kv_ring *self = &g_ring;
+    if (info->msg_type == KV_NODE_INFO_READ) {
+        assert(self->thread_id == kv_app_get_thread_index());
+        node_handler(info);
+    } else {
+        kv_app_send_without_token(self->thread_id, node_handler, info);
     }
 }
 
@@ -138,13 +201,18 @@ static int kv_etcd_poller(void *arg) {
     return 0;
 }
 
-void kv_ring_init(char *etcd_ip, char *etcd_port, uint32_t thread_num) {
+void kv_ring_init(char *etcd_ip, char *etcd_port, uint32_t thread_num, kv_ring_cb ready_cb, void *arg) {
     struct kv_ring *self = &g_ring;
+    self->ready_cb = ready_cb;
+    self->arg = arg;
     self->nodes = NULL;
     self->rings = NULL;
+    self->thread_id = kv_app_get_thread_index();
     pthread_mutex_init(&self->rings_lock, NULL);
-    kvEtcdInit(etcd_ip, etcd_port, node_handler, NULL);
     kv_rdma_init(&self->h, thread_num);
+    kvEtcdInit(etcd_ip, etcd_port, _node_handler, NULL);
+    STAILQ_INIT(&self->conn_q);
+    kv_app_poller_register(kv_conn_q_poller, self, 1000000);
 }
 
 struct vid_ring_stat {
@@ -157,11 +225,13 @@ static int vid_ring_stat_cmp(const void *_a, const void *_b) {
     return a->size < b->size ? -1 : 1;
 }
 
-void kv_ring_server_init(char *local_ip, char *local_port, uint32_t vid_num, uint32_t vid_per_ssd, uint32_t ssd_num) {
+void kv_ring_server_init(char *local_ip, char *local_port, uint32_t vid_num, uint32_t vid_per_ssd, uint32_t ssd_num,
+                         uint32_t con_req_num, uint32_t max_msg_sz, kv_rdma_req_handler handler, void *arg) {
     assert(ssd_num != 0);
     assert(vid_per_ssd * ssd_num <= vid_num);
     assert(local_ip && local_port);
     struct kv_ring *self = &g_ring;
+    kv_rdma_listen(self->h, local_ip, local_port, con_req_num, max_msg_sz, handler, arg);
     self->local_ip = local_ip;
     self->local_port = local_port;
     vid_ring_init(vid_num);
@@ -181,7 +251,6 @@ void kv_ring_server_init(char *local_ip, char *local_port, uint32_t vid_num, uin
     kvEtcdCreateNode(info, 1);
     free(info);
     kv_app_poller_register(kv_etcd_poller, NULL, 300000);
-    // kv_rdma_listen
 }
 
 void kv_ring_fini(void) {
--- kv/kv_ring.h
+++ kv/kv_ring.h
@@ -1,8 +1,12 @@
 #ifndef _KV_RING_H_
 #define _KV_RING_H_
 #include "kv_rdma.h"
+typedef void (*kv_ring_cb)(void *arg);
 void kv_ring_dispatch(char *key, connection_handle *h, uint32_t *ssd_id);
-void kv_ring_init(char *etcd_ip, char *etcd_port, uint32_t thread_num);
-void kv_ring_server_init(char *local_ip, char *local_port, uint32_t vid_num, uint32_t vid_per_ssd, uint32_t ssd_num);
+// client: kv_ring_init(kv_rdma_init)
+// server: kv_ring_init(kv_rdma_init)->kv_ring_server_init(kv_rdma_listen)
+void kv_ring_init(char *etcd_ip, char *etcd_port, uint32_t thread_num, kv_ring_cb ready_cb, void *arg);
+void kv_ring_server_init(char *local_ip, char *local_port, uint32_t vid_num, uint32_t vid_per_ssd, uint32_t ssd_num,
+                         uint32_t con_req_num, uint32_t max_msg_sz, kv_rdma_req_handler handler, void *arg);
 void kv_ring_fini(void);
 #endif
